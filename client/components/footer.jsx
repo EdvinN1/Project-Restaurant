060506ac91b19b90a4eb9e72e67be5ffbb15b949
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-03-30 11:47:33 +0200

    added first section in footer

---
+++ client/components/footer.jsx
@@ -0,0 +1,18 @@
+import '../styling/footer.css'
+
+export default function () {
+  return (
+    <footer>
+      <div className="section1">
+        <h2>Kyoto Hut</h2>
+        <p>
+          Kyoto Hut: Your go-to restaurant for the best sushi pizza in town! Our
+          unique fusion of Japanese and Italian cuisine creates a burst of
+          flavor and texture in every bite. With fresh, high-quality ingredients
+          and a variety of options to choose from, you're sure to satisfy your
+          cravings at Kyoto Hut.
+        </p>
+      </div>
+    </footer>
+  );
+}
