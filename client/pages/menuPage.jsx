de3cde030cc13ff00eaa1fc72f89b0a2d705503d
EdvinN1 <nedde14@gmail.com>
2023-03-30 16:23:43 +0200

    more changes

--- client/pages/menuPage.jsx
+++ client/pages/menuPage.jsx
@@ -7,7 +7,7 @@ export default function () {
 		<h1>MENU</h1>
 	</header>
 	<nav>
-		<ul>
+		<ul className="menuList">
 			<li><Link to = {"/starters"}>Starters</Link></li>
 			<li><Link to={"/mains"}>Mains</Link></li>
 			<li><Link to={"/desserts"}>Desserts</Link></li>
@@ -17,70 +17,70 @@ export default function () {
 	<main>
 		<div id="starters">
 			<h2>Starters</h2>
-			<ul>
+			<ul className="menuList">
 				<li>
 					<h3>Nått gott</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, fugit.</p>
 					<p>Price: $500</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 				<li>
 					<h3>Vitlöksbröd</h3>
 					<p>Salt som snus</p>
 					<p>Price: $7.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
 		<div id="mains">
 			<h2>Mains</h2>
-			<ul>
+			<ul className="menuList">
 				<li>
 					<h3>Vesuvio rolls</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste beatae optio eveniet provident ipsam vitae quidem officia asperiores obcaecati delectus.</p>
 					<p>Price: $12.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 				<li>
 					<h3>kebab sushi special</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, id.</p>
 					<p>Price: $14.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
 		<div id="desserts">
 			<h2>Desserts</h2>
-			<ul>
+			<ul className="menuList">
 				<li>
 					<h3>kladdekaga</h3>
 					<p>krämig o go</p>
 					<p>Price: $8.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 				<li>
 					<h3>grillchips</h3>
 					<p></p>
 					<p>Price: $6.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
     <div id="drinks">
 			<h2>drinks</h2>
-			<ul>
+			<ul className="menuList">
 				<li>
 					<h3>cola
           </h3>
 					<p>krämig o go</p>
 					<p>Price: $8.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 				<li>
 					<h3>fanta</h3>
 					<p></p>
 					<p>Price: $6.99</p>
-					<button>Add to Cart</button>
+					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/footer';
 
 
 
@@ -7,6 +8,7 @@ function App() {
     <>
     {/* <Navbar /> */}
     <Outlet />
+    <Footer />
     
     </>
   )
