5c8fc3a7367f28236f02838a143adae29cbf0b65
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-03-30 11:10:17 +0200

    fixed routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+import {  
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <RouterProvider router={
+          createBrowserRouter(
+              createRoutesFromElements(
+                  <Route path="/" element={<App />}>
+              
+                    {/* <Route index element={<Home />}></Route>    */}
+
+                  </Route>
+              )
+          )
+      } />
+  </React.StrictMode>
+);
