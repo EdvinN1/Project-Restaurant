be67d55d101ad9d184191d205ed4c897c5295b42
EdvinN1 <nedde14@gmail.com>
2023-03-30 10:03:52 +0200

    First commit in my life

---
+++ server/api.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const api = express()
+
+api.use(express.json())
+
+import mongoose from 'mongoose'
+const conn = "mongodb+srv://projekt:12345@cluster.oduwork.mongodb.net/test"
+
+api.listen(3000, () => {
+    console.log(`http://localhost:3000`)
+    mongoose.connect(conn, {dbName: 'project'})
+    .catch((err) => {
+        console.log("Error connecting to database: ", err)
+      })
+})
+
+
+api.get('/', async (request, response) => {
+
+    response.json({"result": "created"})
+
+})
