d8d38109859a342e280a0c29ef598c7cb5fc8e45
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-03-30 23:25:29 +0200

    added more pics, faded-thing and css

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,8 +5,11 @@ export default function(){
         <section class="wrapper">
             <h1>KYOTO HUT</h1>
             <p class="paragraph">We mix the best of two worlds, pizza and sushi!</p>
-            {/* <div class="backgroundImg">  </div> */}
-            <img class="backgroundImg" src="../img/picture1.jpg"></img>
+                <img class="slide1" src="../img/picture1.jpg"></img>
+                <img class="slide2" src="../img/picture2.jpg"></img>
+                <img class="slide3" src="../img/picture3.jpg"></img>
+                <img class="slide4" src="../img/picture4.jpg"></img>
+                <img class="slide5" src="../img/picture5.jpg"></img>
         </section>
     )
 }
