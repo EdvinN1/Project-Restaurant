b91ac955e88cb2f4d9824fe3a2eedb47c4b08fca
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-03-30 16:29:15 +0200

    changed some small things

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,7 +4,7 @@ export default function(){
     return(
         <section class="wrapper">
             <h1>KYOTO HUT</h1>
-            <p class="paragraf">We mix the best of two worlds, pizza and sushi!</p>
+            <p class="paragraph">We mix the best of two worlds, pizza and sushi!</p>
             {/* <div class="backgroundImg">  </div> */}
             <img class="backgroundImg" src="../img/picture1.jpg"></img>
         </section>
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
-
+import Footer from '../components/footer.jsx';
 
 function App() {
   return (
     <>
     {/* <Navbar /> */}
     <Outlet />
+    <Footer />
     </>
   )
 }
