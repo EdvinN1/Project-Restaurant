d3f489200bb0d1a4e7b21b17a7aab0a44fb2372a
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-03-30 12:04:12 +0200

    picture and text added to homepage

---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+export default function(){
+    return(
+        <body style={{ 
+            backgroundImage: 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Sushi_pizza_%281%29.jpg/1920px-Sushi_pizza_%281%29.jpg")',
+            backgroundSize: '100% 100%',
+            width: '1200px',
+            opacity: "1.0"
+          }}>
+              <h1>KYOTO HUT</h1>
+              <p>We mix the best of two worlds, pizza and sushi!</p>
+          </body>
+    )
+}
