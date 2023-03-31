a98ae810fccd65e6e191febfa39721eb0ca0ca1b
EmilNilsson00 <emilnilsson00@icloud.com>
2023-03-31 15:41:39 +0200

    First version of the shopping cart

---
+++ client/pages/shoppingCartPage.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import '../styling/shopping-cart.css';
+
+export default function () {
+    const [items, setItems] = useState(['Product 1', 'Product 2', 'Product 3']);
+    const [newItem, setNewItem] = useState('');
+
+    const handleNewItemChange = (event) => {
+        setNewItem(event.target.value);
+    };
+
+    const handleAddItem = () => {
+        setItems([...items, newItem]);
+        setNewItem('');
+    };
+
+    const handleRemoveItem = (index) => {
+        const newItems = [...items];
+        newItems.splice(index, 1);
+        setItems(newItems);
+    };
+
+    return (
+        <section className="shopping-cart-page">
+            <section></section>
+            <h1 className="shopping-cart__title">Shopping Cart</h1>
+            <section className={"shopping-cart-items"}>
+                <ul className="shopping-cart__list">
+                    {items.map((item, index) => (
+                        <li key={index} className="shopping-cart__item">
+                            {item}{' '}
+                            <button className="shopping-cart__button" onClick={() => handleRemoveItem(index)}>Remove
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <section className={"checkout-section"}>
+                <p className={"total-cost-text"}>Total cost: 0$</p>
+                <button className="shopping-cart__button shopping-cart__button--checkout">Checkout</button>
+            </section>
+        </section>
+    );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,25 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Home from '../pages/homePage.jsx'
 import AboutUs from '../pages/aboutUsPage'
+import ShoppingCart from '../pages/shoppingCartPage'
 
-import {  
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements
 } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <RouterProvider router={
-          createBrowserRouter(
-              createRoutesFromElements(
-                  <Route path="/" element={<App />}>
-                  <Route index element={<Home />}></Route>    
-                  <Route path='/about-us' element={<AboutUs />}></Route>
-                  </Route>
-              )
-          )
-      } />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={
+            createBrowserRouter(
+                createRoutesFromElements(
+                    <Route path="/" element={<App/>}>
+                        <Route index element={<Home/>}></Route>
+                        <Route path='/about-us' element={<AboutUs/>}></Route>
+                        <Route path='/cart' element={<ShoppingCart/>}></Route>
+                    </Route>
+                )
+            )
+        }/>
+    </React.StrictMode>
 );
