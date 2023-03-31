2a2898be41c2b5f73f0b6dd9c1109e15a70101f8
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-03-31 10:52:43 +0200

    content for about us added

--- client/pages/aboutUsPage.jsx
+++ client/pages/aboutUsPage.jsx
@@ -1,7 +1,41 @@
-export default function() {
-    return (
-        <section>
-            
-        </section>
-    )
+export default function () {
+  return (
+    <section className="section-about">
+      <h1>About Us!</h1>
+      <p>
+        Welcome to Kyoto Hut, where we serve the most delicious sushi pizza in
+        the world! Our restaurant is a culinary destination that offers a unique
+        dining experience that combines the best of Japanese and Italian
+        cuisine.
+      </p>
+      <br></br>
+      <p>
+        At Kyoto Hut, we are committed to using only the freshest and highest
+        quality ingredients to create our dishes. Our sushi pizza, in
+        particular, is made with premium sushi-grade fish, fresh vegetables, and
+        a specially crafted dough that is both crispy and chewy.
+      </p>
+      <br></br>
+      <p>
+        We take great pride in our menu, which features a wide variety of
+        traditional Japanese dishes as well as innovative fusion creations. From
+        our classic sushi rolls to our signature sushi pizza, every dish is made
+        with care and attention to detail.
+      </p>
+      <br></br>
+      <p>
+        Our restaurant is staffed by a team of skilled chefs and friendly
+        servers who are passionate about food and dedicated to providing our
+        guests with a memorable dining experience. We strive to create a warm
+        and welcoming atmosphere where customers can relax, unwind, and enjoy
+        great food and conversation.
+      </p>
+      <br></br>
+      <p>
+        Whether you are a sushi aficionado or simply looking to try something
+        new and exciting, we invite you to come and experience the magic of
+        Kyoto Hut. We promise that you won't be disappointed!
+      </p>
+    </section>
+  );
 }
