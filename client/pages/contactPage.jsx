5e19b0ff0502868d39327cc3e446c80b74202352
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-03-31 15:30:19 +0200

    added some css

--- client/pages/contactPage.jsx
+++ client/pages/contactPage.jsx
@@ -1,41 +1,53 @@
-export default function() {
-    return (
-        <section>
-        <div className="container-contact">
-            <h2>Get in touch!</h2>
-            <p>If you have any questions or need help, please fill out the form below. We do our best to respond within 1 business day.</p>
-        </div>    
-        <div className="contact-box">
-            <div className="contact-left">
-                <h3>Send your request</h3>
-                <form>
-                    <div className="input-row">
-                        <div className="input-group">
-                            <label>Name</label>
-                            <input type="text" placeholder="John Doe"></input>
-                        </div>
-                        <div className="input-group">
-                            <label>Phone</label>
-                            <input type="text" placeholder="+46"></input>
-                        </div>        
-                    </div>
+import "../styling/contact.css";
 
-                    <div className="input-row">
-                        <div className="input-group">
-                            <label>Email</label>
-                            <input type="text" placeholder="john.doe@gmail.com"></input>
-                        </div>
-                        <div className="input-group">
-                            <label>Subject</label>
-                            <input type="text" placeholder="Enter a subject"></input>
-                        </div>        
-                    </div>
-                </form>
-            </div>
-            <div className="contact-right">
-                <h3>Reach us here</h3>
-            </div>
+export default function () {
+  return (
+    <section className="section-contact">
+      <div className="container-contact">
+        <h2>Get in touch!</h2>
+        <p>
+          If you have any questions or need help, please fill out the form
+          below. We do our best to respond within 1 business day.
+        </p>
+
+        <div className="contact-box">
+          <div className="contact-left">
+            <h3>Send your request</h3>
+            <form>
+              <div className="input-row">
+                <div className="input-group">
+                  <label>Name</label>
+                  <input className="input-contact" type="text" placeholder="John Doe"></input>
+                </div>
+                <div className="input-group">
+                  <label>Phone</label>
+                  <input className="input-contact" type="text" placeholder="+46"></input>
+                </div>
+              </div>
+
+              <div className="input-row">
+                <div className="input-group">
+                  <label>Email</label>
+                  <input className="input-contact" type="text" placeholder="john.doe@gmail.com"></input>
+                </div>
+                <div className="input-group">
+                  <label>Subject</label>
+                  <input className="input-contact" type="text" placeholder="Enter a subject"></input>
+                </div>
+              </div>
+
+                <label>Message</label>
+                <textarea rows="5" placeholder="Your message"></textarea>
+                <button className="submit-contact" type="submit">Submit</button>
+
+            </form>
+          </div>
+          <div className="contact-right">
+            <h3>Reach us here</h3>
+          </div>
+          </div>
         </div>
-        </section>
-    )
+      
+    </section>
+  );
 }
