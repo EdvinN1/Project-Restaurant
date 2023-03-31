2bc4fd877d23ae6220f83934c62ca845a39e61e5
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-03-31 14:48:09 +0200

    added some content to contact

--- client/pages/contactPage.jsx
+++ client/pages/contactPage.jsx
@@ -1,7 +1,41 @@
 export default function() {
     return (
         <section>
-            
+        <div className="container-contact">
+            <h2>Get in touch!</h2>
+            <p>If you have any questions or need help, please fill out the form below. We do our best to respond within 1 business day.</p>
+        </div>    
+        <div className="contact-box">
+            <div className="contact-left">
+                <h3>Send your request</h3>
+                <form>
+                    <div className="input-row">
+                        <div className="input-group">
+                            <label>Name</label>
+                            <input type="text" placeholder="John Doe"></input>
+                        </div>
+                        <div className="input-group">
+                            <label>Phone</label>
+                            <input type="text" placeholder="+46"></input>
+                        </div>        
+                    </div>
+
+                    <div className="input-row">
+                        <div className="input-group">
+                            <label>Email</label>
+                            <input type="text" placeholder="john.doe@gmail.com"></input>
+                        </div>
+                        <div className="input-group">
+                            <label>Subject</label>
+                            <input type="text" placeholder="Enter a subject"></input>
+                        </div>        
+                    </div>
+                </form>
+            </div>
+            <div className="contact-right">
+                <h3>Reach us here</h3>
+            </div>
+        </div>
         </section>
     )
 }
