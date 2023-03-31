3dd8862c4e4023f29c2ca584d63201ea006f60d6
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-03-31 11:03:25 +0200

    responsiviness and button on homepage

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,14 +2,15 @@ import "../styling/home.css"
 
 export default function(){
     return(
-        <section class="wrapper">
+        <section className="wrapper">
             <h1>KYOTO HUT</h1>
-            <p class="paragraph">We mix the best of two worlds, pizza and sushi!</p>
-                <img class="slide1" src="../img/picture1.jpg"></img>
-                <img class="slide2" src="../img/picture2.jpg"></img>
-                <img class="slide3" src="../img/picture3.jpg"></img>
-                <img class="slide4" src="../img/picture4.jpg"></img>
-                <img class="slide5" src="../img/picture5.jpg"></img>
+            <p className="paragraph">We mix the best of two worlds, pizza and sushi!</p>
+                <img className="slide1" src="../img/picture1.jpg"></img>
+                <img className="slide2" src="../img/picture2.jpg"></img>
+                <img className="slide3" src="../img/picture3.jpg"></img>
+                <img className="slide4" src="../img/picture4.jpg"></img>
+                <img className="slide5" src="../img/picture5.jpg"></img>
+                <button className="btn">ORDER HERE!</button>
         </section>
     )
 }
