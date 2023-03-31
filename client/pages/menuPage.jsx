e22267f58b877fa75e58fba29f4603508ef2fea0
EdvinN1 <nedde14@gmail.com>
2023-03-31 15:54:49 +0200

    media queries

--- client/pages/menuPage.jsx
+++ client/pages/menuPage.jsx
@@ -3,83 +3,82 @@ import "../styling/menuPage.css"
 export default function () {
     return (
         <section>
-            <header>
-		<h1>MENU</h1>
+            <header id="menu-header">
+		<h1 id="menu-h1">MENU</h1>
 	</header>
-	<nav>
-		<ul className="menuList">
-			<li><Link to = {"/starters"}>Starters</Link></li>
-			<li><Link to={"/mains"}>Mains</Link></li>
-			<li><Link to={"/desserts"}>Desserts</Link></li>
-      <li><Link to={"/drinks"}>Drinks</Link></li>
+	<nav className="menu-nav">
+		<ul className="filter-menu">
+			<li className="menupage-header-cards"><a href="#starters">Starters</a></li>
+			<li className="menupage-header-cards"><a href="#mains">Mains</a></li>
+			<li className="menupage-header-cards"><a href="#desserts">Desserts</a></li>
+      		<li className="menupage-header-cards"><a href="#drinks">Drinks</a></li>
 		</ul>
 	</nav>
-	<main>
-		<div id="starters">
-			<h2>Starters</h2>
+	<main className="menu-main">
+		<div className="menupage-div" id="starters">
+			<h2 className="menu-choices">Starters</h2>
 			<ul className="menuList">
-				<li>
-					<h3>Nått gott</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Nått gott</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, fugit.</p>
 					<p>Price: $500</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
-				<li>
-					<h3>Vitlöksbröd</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Vitlöksbröd</h3>
 					<p>Salt som snus</p>
 					<p>Price: $7.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
-		<div id="mains">
-			<h2>Mains</h2>
+		<div className="menupage-div" id="mains">
+			<h2 className="menu-choices">Mains</h2>
 			<ul className="menuList">
-				<li>
-					<h3>Vesuvio rolls</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste beatae optio eveniet provident ipsam vitae quidem officia asperiores obcaecati delectus.</p>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Vesuvio rolls</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, id.</p>
 					<p>Price: $12.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
-				<li>
-					<h3>kebab sushi special</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Kebab sushi special</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, id.</p>
 					<p>Price: $14.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
-		<div id="desserts">
-			<h2>Desserts</h2>
+		<div className="menupage-div" id="desserts">
+			<h2 className="menu-choices">Desserts</h2>
 			<ul className="menuList">
-				<li>
-					<h3>kladdekaga</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Kladdekaga</h3>
 					<p>krämig o go</p>
 					<p>Price: $8.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
-				<li>
-					<h3>grillchips</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Grillchips</h3>
 					<p></p>
 					<p>Price: $6.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
 		</div>
-    <div id="drinks">
-			<h2>drinks</h2>
+    <div className="menupage-div" id="drinks">
+			<h2 className="menu-choices">Drinks</h2>
 			<ul className="menuList">
-				<li>
-					<h3>cola
-          </h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">Cola</h3>
 					<p>krämig o go</p>
 					<p>Price: $8.99</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
-				<li>
-					<h3>fanta</h3>
+				<li className="menupage-cards">
+					<h3 className="menu-orders">BIIIIIIRA</h3>
 					<p></p>
-					<p>Price: $6.99</p>
+					<p>Price: free</p>
 					<button className="add-to-cart">Add to Cart</button>
 				</li>
 			</ul>
