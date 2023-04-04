801a047b98a8b7fb45c552bdbd6e0bd110cc0fd2
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-04-05 01:23:59 +0200

    works with accepting and declining orders, and also ready

--- client/src/components/adminSettings.jsx
+++ client/src/components/adminSettings.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 
-export default function(){
+export default function(props){
 const [orders, setOrders] = useState([]);
+/* const [acceptedOrders, setAcceptedOrders] = useState(props.acceptedOrders); */
+/* const [aOrders, setAOrders] = useState([]); */
 
+//get all orders
 useEffect(() => {
     fetch('http://localhost:3000/api/admin')
         .then(response => response.json())
@@ -10,34 +13,57 @@ useEffect(() => {
         .catch(error => console.error(error))
 }, []);
 
-function handleDeclineClick(inDate){
+function handleAcceptClick(inData){
+    // Find the index of the item with the matching ID
+    const index = orders.findIndex(item => item.orderID === inData);
+    
+    if (index >= 0) {
+      // Remove the item from the source list using splice()
+      const [removedItem] = orders.splice(index, 1);
+      console.log("id: " + removedItem.orderID);
+      /* const updatedAcceptedOrders = [...props.acceptedOrders, removedItem]; */
+      props.updateAcceptedOrders(removedItem);
+      /* console.log("orderID: " + props.acceptedOrders[0].orderID); */
+      // Add the removed item to the target list using concat()
+     /*  props.updateAcceptedOrders([...props.acceptedOrders, removedItem]); */
+        
+      // Update the source list with the modified array
+      setOrders([...orders]);
+    }
+  };
+
+function handleDeclineClick(inData){
     console.log("delete");
-    fetch(`http://localhost:3000/api/admin/orders?orderID=${inDate}`, {
+    fetch(`http://localhost:3000/api/admin/orders?orderID=${inData}`, {
         method: 'DELETE'
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
+    //to update the state and filter out inDate
+    .then(response => {
+        if(response.ok) {
+            setOrders(orders.filter(order => order.orderID !== inData))
+        } else {
+            console.error('Response not ok')
+        }
+    })
     .catch(error => console.error(error));
-/*     fetch('http://localhost:3000/api/admin', {
-        method: 'DELETE'
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error)); */
 }
-
     return (
         <div>
             <h4>current orders</h4>
-            {orders.map(order => (
-                <div>
-                    <p>orderID: {order.orderID}</p>
-                    <p>restaurantID: {order.restaurantID}</p>
-                    <p>orderDate: {order.orderDate}</p>
-                    <button className="adminBtn">accept</button>
-                    <button className="adminBtn" onClick={() => handleDeclineClick(order.orderID)}>decline</button>
-                </div>
-            ))}
+            <div>
+                {orders.map(order => (
+                    <div>
+                        <p>orderID: {order.orderID}</p>
+                        <p>restaurantID: {order.restaurantID}</p>
+                        <p>orderDate: {order.orderDate}</p>
+                        <button className="adminBtn" onClick={() => handleAcceptClick(order.orderID)}>accept</button>
+                        <button className="adminBtn" onClick={() => handleDeclineClick(order.orderID)}>decline</button>
+                    </div>
+                ))}
+            </div>
+            <div>
+                testar hejsan
+            </div>
         </div>
     )
 }
--- client/src/pages/adminDashboard.jsx
+++ client/src/pages/adminDashboard.jsx
@@ -10,6 +10,7 @@ import AdminSettings from "../components/adminSettings";
 
 export default function(){
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [acceptedOrders, setAcceptedOrders] = useState([]);
 
     const categoryComponents = {
         "Users": <AdminUsers category="Users" />,
@@ -17,12 +18,32 @@ export default function(){
         "Entrée": <AdminEntree category="Entrée" />,
         "Desserts": <AdminDesserts category="Desserts" />,
         "Orders": <AdminOrders category="Orders" />,
-        "Settings": <AdminSettings category="Settings"/>
+        "Settings": <AdminSettings category="Settings" updateAcceptedOrders={updateAcceptedOrders} acceptedOrders={acceptedOrders}/>
     }
+
+    function updateAcceptedOrders(order) {
+        setAcceptedOrders(prevAcceptedOrders => [...prevAcceptedOrders, order]);
+      }
 
     function handleCategoryClick(categoryName) {
         setSelectedCategory(categoryName);
       }
+
+      function handleReadyClick(inData){
+        console.log("delete");
+        fetch(`http://localhost:3000/api/admin/orders?orderID=${inData}`, {
+            method: 'DELETE'
+        })
+        //to update the state and filter out inDate
+        .then(response => {
+            if(response.ok) {
+                setAcceptedOrders(acceptedOrders.filter(acceptedOrders => acceptedOrders.orderID !== inData))
+            } else {
+                console.error('Response not ok')
+            }
+        })
+        .catch(error => console.error(error));
+    }
 
     return(
         <section className="adminWrapper">
@@ -42,6 +63,15 @@ export default function(){
                 </div>
                 <div className="item3">
                 Some info here
+                {acceptedOrders.map(order => (
+                <div>
+                {/* {console.log("orderID: " + order.orderID)} */}
+                    <p>orderID: {order.orderID}</p>
+                    <p>restaurantID: {order.restaurantID}</p>
+                    <p>orderDate: {order.orderDate}</p>
+                    <button className="adminBtn" onClick={() => handleReadyClick(order.orderID)}>Ready</button>
+                    </div>
+                ))}
                 </div>
             </div>
         </section>
