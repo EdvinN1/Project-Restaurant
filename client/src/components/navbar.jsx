9c248eaf92fed5d76a39db4f58f3ad9dba20129d
EdvinN1 <nedde14@gmail.com>
2023-04-04 15:22:46 +0200

    menu route

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,43 +1,43 @@
-import {useRef} from "react";
-import {Link} from "react-router-dom";
-import "../styling/navbar.css"
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import "../styling/navbar.css";
 
 export default function () {
-    const navRef = useRef();
+  const navRef = useRef();
 
-    const showNavbar = () => {
-        navRef.current.classList.toggle(
-            "responsive_nav"
-        );
-    };
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    return (
-        <header className={"navbar-header"}>
-            <h3>KYOTO HUT</h3>
+  return (
+    <header className={"navbar-header"}>
+      <h3>KYOTO HUT</h3>
 
-            <nav className={"navbar-nav"} ref={navRef}>
-                    <Link className={"link-buttons"} to="/">Home</Link>
-                    <Link className={"link-buttons"} to="/">Menu</Link>
-                    <Link className={"link-buttons"} to="/contact">Contact</Link>
-                    <Link className={"link-buttons"} to="/about-us">About us</Link>
-                    <button
-
-                        className="nav-btn nav-close-btn"
-                        onClick={showNavbar}>
-                        <i className="material-icons">close</i>
-
-                    </button>
-            </nav>
-                <div className={"shopcart-hmbrgr-icons-wrapper"}>
-                    <button className={"shopping-cart-button"}>
-                        <i className="material-icons">shopping_cart</i>
-                    </button>
-                    <button
-                        className="nav-btn"
-                        onClick={showNavbar}>
-                        <i className="material-icons hamburger-menu">menu</i>
-                    </button>
-                </div>
-        </header>
-);
+      <nav className={"navbar-nav"} ref={navRef}>
+        <Link className={"link-buttons"} to="/">
+          Home
+        </Link>
+        <Link className={"link-buttons"} to="/Menu">
+          Menu
+        </Link>
+        <Link className={"link-buttons"} to="/contact">
+          Contact
+        </Link>
+        <Link className={"link-buttons"} to="/about-us">
+          About us
+        </Link>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <i className="material-icons">close</i>
+        </button>
+      </nav>
+      <div className={"shopcart-hmbrgr-icons-wrapper"}>
+        <button className={"shopping-cart-button"}>
+          <i className="material-icons">shopping_cart</i>
+        </button>
+        <button className="nav-btn" onClick={showNavbar}>
+          <i className="material-icons hamburger-menu">menu</i>
+        </button>
+      </div>
+    </header>
+  );
 }
