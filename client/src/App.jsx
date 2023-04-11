99f4dc6b84cc1b12a5184e53a6ccb5c881a3c97d
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-04-11 14:22:27 +0200

    session works now when reloading the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,35 @@
 import Navbar from './components/navbar'
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/footer.jsx';
-import {useStates, useAutoKeys, useDebug} from 'react-easier';
+import { useStates, useAutoKeys, useDebug } from 'react-easier';
 import { GlobalProvider } from './GlobalContext';
+import { useEffect } from 'react';
 
 function App() {
-    useAutoKeys();
-    useDebug();
-    useStates("access", {
-        admin: false,
-        loggedIn: false
-    })
-    useStates("cart",[])
-    return (
-        <GlobalProvider>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
-        </GlobalProvider>
-    )
+  useAutoKeys();
+  useDebug();
+  useStates("access", {
+    admin: false,
+    loggedIn: false
+  })
+  useStates("cart", [])
+  const access = useStates("access")
+  useEffect(() => {
+    const loggedIn = sessionStorage.getItem("loggedIn");
+    if (loggedIn) {
+      const admin = sessionStorage.getItem("admin") === "true";
+      access.admin = admin;
+      access.loggedIn = loggedIn;
+    }
+  }, [])
+
+  return (
+    <GlobalProvider>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </GlobalProvider>
+  )
 }
 
-export default App
+export default App;
